2f81c8e00c0f2a0504acc286f2b211b1a41664a0
Овчинников Игорь <faceplaces@inbox.ru>
2023-07-20 22:23:54 +0300

    third commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,15 +1,17 @@
 export default class App {
   constructor(data) {
     this.movies_list = data;
+    this.table = 0;
     this.createTable();
+    this.readData(data);
   }
 
   createTable() {
-    let table = document.createElement("table");
-    document.querySelector("body").appendChild(table);
-    table = document.querySelector("table");
+    this.table = document.createElement("table");
+    document.querySelector("body").appendChild(this.table);
     const row = document.createElement("tr");
-    table.appendChild(row);
+    row.classList.add("table-header");
+    this.table.appendChild(row);
     for (const title of ["id", "title", "year", "imdb"]) {
       const cell = document.createElement("td");
       cell.classList.add(title);
@@ -18,11 +20,35 @@ export default class App {
     }
   }
 
-  readingData(list) {
+  readData(list) {
     for (const item of list) {
-      for (const key of Object.keys(item)) {
-        
-      }
+      const row = document.createElement("tr");
+      row.dataset.id = item.id;
+      row.dataset.title = item.title;
+      row.dataset.year = item.year;
+      row.dataset.imdb = item.imdb;
+      let cell = document.createElement("td");
+      cell.innerText = `${item.id}\u2193`;
+      row.appendChild(cell);
+      cell = document.createElement("td");
+      cell.innerText = `${item.title}\u2191`;
+      row.appendChild(cell);
+      cell = document.createElement("td");
+      cell.innerText = `(${item.year})`;
+      row.appendChild(cell);
+      cell = document.createElement("td");
+      cell.innerText = `imdb: ${new Intl.NumberFormat("ru", {
+        style: "decimal",
+        minimumFractionDigits: 2
+      }).format(item.imdb)}`;
+      row.appendChild(cell);
+      this.table.appendChild(row);
     }
+  }
+
+  sort() {
+    setInterval(() => {
+
+    }, 2000);
   }
 }
